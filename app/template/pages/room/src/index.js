328a417a9a96e9f81dd64ec5b38fda173f81d0e3
flaviosenne123 <joaoflausino123@hotmail.com>
2021-05-18 19:34:24 -0300

    create class socket builder

---
+++ app/template/pages/room/src/index.js
@@ -0,0 +1,9 @@
+const room = {
+    id: Date.now(),
+    topic: 'JS Expert'
+}
+
+const user = {
+    img: 'https://www.iconfinder.com/icons/4043232/avatar_batman_comics_hero_icon',
+    username: 'Joao Flavio'
+}
